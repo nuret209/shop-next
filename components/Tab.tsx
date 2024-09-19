626409a3a0e4f4816a9bcc510ce8c8ab08b5eeda
nuret209 <noguc4200@gmail.com>
2024-09-19 17:56:20 +0300

    commit 5

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -13,7 +13,9 @@ function Tab({defaultTab, a , TabSVGS, TabTitles} : TabProps) {
     const [active, setactive] = React.useState(defaultTab);
     return (
         <>
-            {TabTitles.map((title , index : number) => <div key={index} className={
+            {TabTitles.map((title , index : number) => <div 
+            key={index} 
+            className={
                 `transition-all text-[10px] whitespace-nowrap flex-col sm:flex-row cursor-pointer duration-200 p-[12px] w-1/4 flex sm:text-[14px] items-center justify-center text-[#868c98] rounded-[100px] no-underline stroke-[#868c98]
                  ${active == index && "text-[#0a0d14] px-[16px] py-[12px] bg-[#fff] [box-shadow:0px_6px_10px_rgba(27,_28,_29,_0.06),_0px_2px_4px_rgba(27,_28,_29,_0.02)]" }`
                 } onClick={() => { setactive(index); a(index + 1) }}>
